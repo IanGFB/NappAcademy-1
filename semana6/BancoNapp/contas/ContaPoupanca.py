332d79cd68ed0a06123a9b3eefc4c7d76cd12638
IanGFB <65233519+IanGFB@users.noreply.github.com>
2021-03-14 12:03:26 -0300

    Update ContaPoupanca.py

--- semana6/BancoNapp/contas/ContaPoupanca.py
+++ semana6/BancoNapp/contas/ContaPoupanca.py
@@ -2,4 +2,13 @@ from BancoNapp.contas.Conta import Conta
 
 
 class ContaPoupanca(Conta):
-   pass
+   def __init__(self,  **kwargs):
+      "Classe Conta Poupanca"
+      super(ContaPoupanca, self).__init__(**kwargs)
+      self.profissao = kwargs.get('profissao', '')
+      self.limite = kwargs.get('limite', 0)
+
+   def rendimento_aniversario(self, valor):
+      if valor < 0 or valor > 1:
+         raise ValueError('Os juros precisam ser entre 0 (0%) e 1 (100%).')
+      self.saldo += self.saldo*valor
